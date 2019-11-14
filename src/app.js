8300c8affd561376c6e0d85b6c97100e69708fd5
Juliano Sirtori <julianosirtori@gmail.com>
2019-11-14 06:27:24 -0300

    Add cors

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import express from 'express';
 import Youch from 'youch';
 import 'express-async-errors';
 import path from 'path';
+import cors from 'cors';
 import * as Sentry from '@sentry/node';
 import routes from './routes';
 
@@ -24,6 +25,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
